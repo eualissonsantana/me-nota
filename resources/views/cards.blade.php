4ebcdff6e3a9d2d32b3333cf17c5bfffef278a7c
Alisson Santana <alissonsantanapo@gmail.com>
2020-03-19 23:37:58 -0300

    Página home
    Início da construção da página home

---
+++ resources/views/cards.blade.php
@@ -0,0 +1,56 @@
+
+
+<div>
+    @yield('teste')
+</div>
+
+<article class="row">
+    <section class="container box tarefas-do-dia">
+        
+        <section class="titulo-box">
+            <h3>Para hoje</h3>
+            <hr>
+        </section>
+                 
+        <!--POSTS-->
+        <article class="post container-fluid">
+            
+            <article class="row d-flex">
+                <div class="col-8">
+                    <h4></h4>
+                </div>
+                <div class="col-4">
+                    <div class="icones row">
+                        <div class="">
+                            <a class="icon"  href="#>"><img src="img/icons/edit-file.png" width="25"></a>
+                        </div>
+                        <div class="">
+                            <a class="icon" href="#"><img src="img/icons/remove-file.png" width="25"></a>
+                        </div>
+                    </div>
+                </div>
+            </article>
+            <hr>
+            <p></p>
+            <section class="row">
+                <section class="col-4 rodape d-flex justify-content-start">
+                    <article class="tipo">
+                        <h5></h5>
+                    </article>
+                </section>
+                <section class="col-8 ">
+                    <article class="row rodape d-flex justify-content-end">
+                        <article class="time ">
+                            <h5 id="data"></h5>
+                        </article>
+                        <article class="time ">
+                            <h5></h5>
+                        </article>
+                    </article>
+                </section>
+            </section>
+        </article>
+            
+        
+    </section>
+</article>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,32 @@
 @extends('layouts.app')
 
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
+            <section class="container-fluid box-geral">
+                
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    You are logged in!
                 </div>
-            </div>
-        </div>
+
+                <section class="opcoes row d-flex justify-content-center">
+                    <h1 id="nome-user">Olá, {{ Auth::user()->name}}!</h1> 
+                </section>
+                <section class="row d-flex justify-content-init">
+                    <article class="btn-tarefa">
+                        <button type="button" class="btn btn-outline-dark" id="btn-categorias" data-toggle="modal" data-target="#ModalCategoria">+ Categorias</button>
+                        <button type="button" class="btn btn-outline-dark" id="btn-tarefas" data-toggle="modal" data-target="#ModalTarefas">+ Tarefas</button>
+                    </article>
+                </section>
+            </section>
     </div>
 </div>
-@endsection
+@stop
+
+
+
