9f76dadbc0af47af3d8f957fd3b4dd2f3341fa74
Alisson Santana <alissonsantanapo@gmail.com>
2020-03-27 21:27:13 -0300

    Alteração tela Quadros

--- resources/views/quadros.blade.php
+++ resources/views/quadros.blade.php
@@ -1,56 +1,72 @@
 @extends('home')
 
 @section('tarefas')
-<section class="col-10 row d-flex justify-content-between">
+<section class="col-10 row d-flex justify-content-between quadros">
     <section class="card shadow-sm " id="to-do">
         <section class="top-card">
-           <article class="row ">
+           <article class="row d-flex justify-content-between header">
                 <article class="row col-7 title">
                     <img src="\assets\img\circle.png">
                     <h5>A fazer</h5>
                 </article>
-                <article class="col-5 d-flex justify-content-end title">
-                        <img src="\assets\img\add.png">
+                <article class="col-5 d-flex justify-content-end options">
+                    <a href="#"><img src="\assets\img\add.png"id="add-img"></a>
+                    <a href="#"><img src="\assets\img\three-dots.png" id="dots-img"></a>
                 </article>
            </article>
         </section>
         <section class="task-content">
+            <ul>
+                <li class="task-card-row clearfix">
+                   
+                </li>
+            </ul>
         </section>
     </section>
 
 
     <section class="card shadow-sm" id="doing">
         <section class="top-card  ">
-            <article class="row ">
+            <article class="row d-flex justify-content-between header">
                 <article class="row col-7 title">
                     <img src="\assets\img\doing-circle.png">
                     <h5>Em progresso</h5>
                 </article>
-                <article class="col-5 d-flex justify-content-end title">
-                    <img src="\assets\img\add.png">
+                <article class="col-5 d-flex justify-content-end options">
+                    <a href="#"><img src="\assets\img\add.png" id="add-img"></a>
+                    <a href="#"><img src="\assets\img\three-dots.png" id="dots-img"></a>
                 </article>
            </article>
            
         </section>
         <section class="task-content">
-
+            <ul>
+                <li class="task-card-row clearfix">
+                   
+                </li>
+            </ul>
         </section>
     </section>
 
-    <section class="card shadow-sm" id="to-do">
+    <section class="card shadow-sm" id="done">
         <section class="top-card ">
-            <article class="row">
+            <article class="row d-flex justify-content-between header">
                 <article class="row col-7 title">
                     <img src="\assets\img\filled-circle.png">
                     <h5>Concluída</h5>
                 </article>
-                <article class="col-5 d-flex justify-content-end title">
-                    <img src="\assets\img\add.png">
+                <article class="col-5 d-flex justify-content-end options">
+                    <a href="#"><img src="\assets\img\add.png" id="add-img"></a>
+                    <a href="#"><img src="\assets\img\three-dots.png" id="dots-img"></a>
                 </article>
             </article>
         </section>
         <section class="task-content">
-
+            <ul>
+                <li class="task-card-row clearfix">
+                   
+                </li>
+            </ul>
         </section>
     </section>
 
